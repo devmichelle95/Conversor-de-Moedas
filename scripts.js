9e4e615a5eaa234ba19bc99760020fa69bfd5d4f
Michelle Oliveira <28oliveira.michelle@gmail.com>
2023-06-26 15:28:46 -0300

    add async await

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
-const dolar = 5.2
-const euro = 5.9
 
-const convertValue = () => {
+const convertValue = async () => {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+
 
     realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
